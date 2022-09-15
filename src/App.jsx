4555926fced58ee1cfab54d5f1bc6347a0d99d70
reid <reidbras@gmail.com>
2022-09-14 20:57:36 -0500

    Adding code to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
+import "./App.css";
+import { HomePage } from "./pages/index.js";
+import { FilmsPage } from "./pages/index.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+return (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="films">Films</NavLink>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<HomePage></HomePage>}></Route>
+    </Routes>
+  </BrowserRouter>
+);
 export default App;
