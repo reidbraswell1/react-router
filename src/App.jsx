54865ebdec233c98c9de0c3a63d6282e536d45c7
reid <reidbras@gmail.com>
2022-09-16 12:03:35 -0500

    Styling changes and adding exact to try and resolve active link problem

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,31 @@ import { BrowserRouter } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 import "./App.css";
 import { HomePage } from "./pages/index.js";
 import { FilmsPage } from "./pages/index.js";
 
 function App(props) {
+
+  //const { pathname } = useLocation();
+  //console.log(pathname)
+
   return (
     <BrowserRouter>
-      <nav className="navbar navbar-expand-sm navbar-light" style={{backgroundColor:"rgb(51, 62, 84)"}}>
+      <nav className="navbar navbar-expand-sm navbar-light">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <NavLink className="nav-link color-white" style={{color:"white" }}to="/">Home</NavLink>
+            <NavLink exact className="nav-link color-white" activeClassName="active" to="/">Home</NavLink>
           </li>
           <li className="nav-item">
-            <NavLink className="nav-link color-white" to="films">Films</NavLink>
+            <NavLink className="nav-link color-white"  to="/films">Films</NavLink>
           </li>
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route exact path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/films" element={<FilmsPage></FilmsPage>}></Route>
       </Routes>
     </BrowserRouter>
   )
