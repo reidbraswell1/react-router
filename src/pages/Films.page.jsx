6a52ab0e22820305bc4a98943b6f4708a9b81ee4
Reid Braswell <reidbras@gmail.com>
2022-09-16 17:23:39 -0500

    Adding styling and heading for films list

--- src/pages/Films.page.jsx
+++ src/pages/Films.page.jsx
@@ -10,7 +10,7 @@ function FilmsPage(props) {
 
     const [ list, setList ] = useState([]);
     const [ errorText, setErrorText ] = useState("");
-    const [ searchDirector, setSearchDirector ] = useState("");
+    const [ searchDirector, setSearchDirector ] = useState("All");
 
     useEffect(function () {
         console.log(`---Begin useEffect()---`);
@@ -59,28 +59,41 @@ function FilmsPage(props) {
     console.log(`Directors = ${directors}`);
 
     return(
-        <div>
-            <h1>Studio Ghibli Films</h1>
-            <form>
-                <div className="form-group">
-                    <label htmlFor="searchDirector">Director</label>
-                    <select id="searchDirector" value={searchDirector} onChange={(e) => { setSearchDirector(e.target.value)} }>
-                        <option value="All">All</option>
-                        { directors.map((value) => {
-                            return(<option value={value}>{value}</option>);
-                        }) }
-                    </select>
+        <div className="container">
+            <div className="row">
+                <div className="col-4 my-center">
+                    <h1 className="color-white">Studio Ghibli Films</h1>
                 </div>
-            </form>
-        <ul className="list-group">
-            {filmsByDirector.map((value,index,array) => {
-                return(
-                        <li className="list-group-item" 
-                            key={value.id} 
-                            id={value.id}>{index+1}. {value.title} - {value.director}
-                        </li>)})}
-        </ul>
-        <p className="error"><span className="color-red">{errorText}</span></p>
+            </div>
+            <div className="row">
+                <div className="col-4 my-center">
+                    <form className="mt-2 mb-2">
+                        <div className="form-group">
+                            <label className="color-white" htmlFor="searchDirector">Director</label>
+                            <select className="form-select mt-1" id="searchDirector" value={searchDirector} onChange={(e) => { setSearchDirector(e.target.value)} }>
+                                <option value="All">All</option>
+                                {directors.map((value) => {
+                                    return(<option value={value}>{value}</option>);
+                                }) }
+                            </select>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-5 my-center">
+                    <h4 className="text-center color-white">Title - Director</h4>
+                    <ul className="list-group">
+                        {filmsByDirector.map((value,index,array) => {
+                            return(
+                                <li className="list-group-item" 
+                                    key={value.id} 
+                                    id={value.id}>{index+1}. {value.title} - {value.director}
+                                </li>)})}
+                    </ul>
+                    <p className="error"><span className="color-red">{errorText}</span></p>
+                </div>
+            </div>
         </div>);
 
 }
